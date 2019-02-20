a4339a75bc3adac6dbbdbe940fa82db8bf65249b
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-20 16:15:03 -0500

    oh yeah

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var animals = ["penguin","snake","red panda","big-foot"];
-document.getElementById("txt").innerHTML = list1;
 var list1 = "<ol>";
-var animals.forEach(loop);
+animals.forEach(loop);
+list1 += "</ol>";
+document.getElementById("txt").innerHTML = list1;
 var loop = function(d)
 {
   list1 += "<li>"+d+"</li>";
 }
-list1 += "</ol>";
