74d824c9b2c8579f1f3036de63c331b1dd682e17
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-19 21:07:38 -0500

    we'll see

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var animals = ["penguin","snake","red panda","big-foot"];
 animals.write("<ol>")
 var animals.forEach(function(d))
 {
-  animals.write("<li>"+d+"</li>");
+  document.write("<li>"+d+"</li>");
 }
-animals.write("<ol>")
+document.write("<ol>");
