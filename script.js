d9b1a103b0966148105a66cbe3b5ffe501e87785
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-20 15:53:32 -0500

    fixed syntax error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var animals = ["penguin","snake","red panda","big-foot"];
 var list1 = "<ol>";
-var animals.forEach(function(d))
+var animals.forEach(function(i,d,arr))
 {
   list1 += "<li>"+d+"</li>";
 }
