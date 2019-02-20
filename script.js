cb4d60f814d1505480c3adc3f39d3cb034ce961c
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-20 16:11:28 -0500

    this should work

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var animals = ["penguin","snake","red panda","big-foot"];
+document.getElementById("txt").innerHTML = list1;
 var list1 = "<ol>";
-var animals.forEach(function(d))
+var animals.forEach(loop);
+var loop = function(d)
 {
   list1 += "<li>"+d+"</li>";
 }
 list1 += "</ol>";
-document.getElementById("txt").innerHTML = list1;
