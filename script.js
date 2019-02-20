2c9d85b26f18cadc15ae55d3dc3e580d2d4e2d56
Arsenio Benitez Jr <Ar15macbook@Arsenios-MacBook-Pro.local>
2019-02-20 11:04:41 -0500

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var animals = ["penguin","snake","red panda","big-foot"];
-document.write("<ol>")
+document.write("<ol>");
 var animals.forEach(function(d))
 {
   document.write("<li>"+d+"</li>");
 }
-document.write("<ol>")
+document.write("<ol>");
